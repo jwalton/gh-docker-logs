65e38fa5cdefaa06328c5431d4385249e3ee3470
Jason Walton <jwalton@benbria.ca>
2019-09-12 15:23:37 -0400

    fix: Show container status.

--- main.js
+++ main.js
@@ -28,7 +28,9 @@ if (dest) {
     fs.mkdirSync(dest, { recursive: true });
 }
 
-const ps = run('docker ps -a --format "table {{.ID}},{{.Image}},{{.Names}}" --no-trunc');
+const ps = run(
+    'docker ps -a --format "table {{.ID}},{{.Image}},{{.Names}}.{{.Status}}" --no-trunc'
+);
 // `slice(1)` to remove the 'CONTAINER_ID,IMAGE,NAMES' header.
 const psLines = ps.split(/\r?\n/).slice(1);
 
@@ -40,12 +42,13 @@ for (const line of psLines) {
         continue;
     }
 
-    const [containerId, image, containerName] = line.split(',');
+    const [containerId, image, containerName, containerStatus] = line.split(',');
 
     if (!dest) {
         console.log('**********************************************************************');
-        console.log(`* Image: ${image}`);
-        console.log(`* Name : ${containerName}`);
+        console.log(`* Name  : ${containerName}`);
+        console.log(`* Image : ${image}`);
+        console.log(`* Status: ${containerStatus}`);
         console.log('**********************************************************************');
 
         run(`docker logs ${containerId}`, { passthrough: true });
